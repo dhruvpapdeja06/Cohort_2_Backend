31b9a7823bfe48fdf1e285660fbcec567ef36561
dhruvpapdeja06 <dhruv2024papdeja@gmail.com>
2026-02-06 00:44:28 +0530

    Authentication Implementd

--- Sheriyans_Backend/Phase-2/Day-10/Authentication/src/app.js
+++ Sheriyans_Backend/Phase-2/Day-10/Authentication/src/app.js
@@ -3,12 +3,18 @@
 
 const express = require("express")
 const authRouter = require("./routes/auth.routes")
+const cookieParser = require("cookie-parser")
 
 const app = express()
 
 // middleware
 app.use(express.json())
 
+//cookie
+app.use(cookieParser()) 
+
+//authRouter with prefix
+app.use('/api/auth',authRouter)
 
 
 module.exports = app;
--- Sheriyans_Backend/Phase-2/Day-10/Authentication/src/routes/auth.routes.js
+++ Sheriyans_Backend/Phase-2/Day-10/Authentication/src/routes/auth.routes.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const userModel = require("../models/user.model")
-
-
+const jwt = require("jsonwebtoken")
 const authRouter = express.Router()
 
 // other than app.js if we create an API for that  we need an Router
@@ -21,16 +20,47 @@ const authRouter = express.Router()
 // cookies storage at client side --> Server has directy access of it.
 
 // Generally same email --> 2 user create --> but in Real world that's not happen
+
+/**
+ * api/auth/register
+ */
 authRouter.post("/register", async (req,res)=>{
     const {name, email, mobile , password} = req.body
+
+    // Check if one of them any  exist then not able to register
+    const isUserAlreadyExist = await userModel.findOne({
+      $or:[ 
+        {email: email},
+        {mobile: mobile}
+      ]
+    })
+
+    if(isUserAlreadyExist){
+        const conflictField = isUserAlreadyExist.email === email ? "Email" : "Mobile Number";
+        return res.status(409).json({
+            message : `User Already Register with the ${conflictField}`
+        })
+    }
+
 
     const user = await userModel.create({
         name, email, mobile, password
     })
 
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("jwt_token",token)
+
     res.status(201).json({
         message: "User Register successfully",
-        user
+        user,
+        token
     })
 
 })
