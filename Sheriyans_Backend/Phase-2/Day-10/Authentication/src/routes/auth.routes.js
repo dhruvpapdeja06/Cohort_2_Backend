1c2028c35dd81e76a9a9a032993f5a77ea4446c2
dhruvpapdeja06 <dhruv2024papdeja@gmail.com>
2026-02-06 22:37:53 +0530

    Auth , Login API created

--- Sheriyans_Backend/Phase-2/Day-10/Authentication/src/routes/auth.routes.js
+++ Sheriyans_Backend/Phase-2/Day-10/Authentication/src/routes/auth.routes.js
@@ -65,4 +65,59 @@ authRouter.post("/register", async (req,res)=>{
 
 })
 
+// dummy -api --> /protected
+authRouter.post("/protected", (req,res)=>{
+    console.log(req.cookies);
+
+    res.status(200).json({
+        "message" : "This is a protected route"
+    })
+})
+
+//When same user already register and need new token for that again rqst to the server for login
+// Details --> login, password --> different website login , lost the token, browser ma store then secuity issue.
+
+//In token we dont' store sensitive information --> we don't store inside it.
+
+//function ,fat arrow function ,callback function , only then execute when rqst came to website --> controlller.
+
+//Database breach or leak --> meaning -->Data inside DB leak is present on Internet or someone else.It's is worst day of backend developer life then you have to terminate from your job.
+// user login --> based on what --> email,password --hacker has email and password (store as has value)
+
+//md5 hast generator
+
+authRouter("/login", async (req,res)=>{
+    const {email, password} = req.body
+
+    const user = userModel.find({email})
+
+    if(!user){
+        res.status(404).json({
+            message: "user doesn't exist with this email"
+        })
+    }
+
+    const isPasswordMatched = user.password === password
+
+    
+    if(!isPasswordMatched){
+        res.status(401).json({
+            message: "password in invalid"
+        })
+    }
+
+    const token = jwt.sign({
+        id : user._id
+    },
+    process.env.JWT_SECRET
+    )
+
+    res.cookie("jwt_token",token)
+
+    res.status(201).json({
+        message: "Login sucessfully",
+        token
+    })
+})
+
 module.exports = authRouter;
